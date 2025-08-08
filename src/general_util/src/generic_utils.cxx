c2fef225eef5140e8b92a8cfb2a8ef991c53cffb
James Amrhein <james.s.amrhein@nasa.gov>
2025-08-08 16:01:05 -0400

    Give the version of the ScienceTools in the products #65: Added support for general_util/generic_utils.h ’s GenericUtils::creator_banner which will update the CREATOR tag in the file’s header. New submodule to handle creating the new CREATOR string that will be used by CLI and put into file HEADER's CREATOR value.

--- src/general_util/src/generic_utils.cxx
+++ src/general_util/src/generic_utils.cxx
@@ -4,16 +4,17 @@
 #include <iostream>
 #include "config.h"
 
+// Grabs PROJECT_DESCRIPTION and PROJECT_VERSION from ScienceTools/CMakeLists.txt Project tuple.
 std::string GenericUtils::creator_banner(const std::string  creator)  {
     // preset to existing creator string, will return this string if it's already been updated with tool/same version
     std::string creator_string = creator;
-    // New version of creator string
+    // Create new version of creator string
     std::ostringstream creator_version;
 
-    if (creator.find(PROJECT_VERSION) == std::string::npos) {
+    // If the current version number or description isn't found, update the CREATOR string with the latest PROJECT_DESCRIPTION/PROJECT_VERSION.
+    if (creator.find(PROJECT_VERSION) == std::string::npos && creator.find(PROJECT_DESCRIPTION) == std::string::npos) {
        creator_version << creator << " " << PROJECT_DESCRIPTION << " " << PROJECT_VERSION;
        creator_string = creator_version.str();
-       std::cout << "Creator banner set to " << creator_string << std::endl; 
     }
     return creator_string;
 }
