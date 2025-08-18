85caa430ec2e9a8f4dd9aa6b92be4900c458afb3
jasercion <jasercion.gwu@gmail.com>
2025-08-18 10:35:45 -0400

    update and clean up build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,21 +5,12 @@ export condaname="fermitools"
 
 if [ "$(uname)" == "Darwin" ]; then
     # If Mac OSX then set sysroot flag (see conda_build_config.yaml)
-    export CXXFLAGS="-mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET} -std=c++17 ${CXXFLAGS}" 
-    #echo "CXX FLAGS:"
-    #echo $CXXFLAGS
-    # -I/Applications/Xcode_15.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/include/c++/4.2.1/"
+    #export CXXFLAGS="-mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET} -std=c++17 ${CXXFLAGS}" 
+    export CXXFLAGS="-std=c++17 ${CXXFLAGS}" 
     export LDFLAGS="${LDFLAGS}  -lstdc++ -headerpad_max_install_names"
-    #echo "LD FLAGS:"
-    #echo $LDFLAGS
-    # -L/Applications/Xcode_15.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/include/c++/4.2.1/
-    export TOOLCHAIN_FILE="$RECIPE_DIR/toolchain/cross-osx.cmake"
-    # echo "Commandline tools:" 
-    # ls /Applications/Xcode_15.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/include/c++/4.2.1
-    # export CC="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang"
-    # export CXX="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++"
+    export TOOLCHAIN_FILE="cmake/cross-osx.cmake"
 else
-    export TOOLCHAIN_FILE="${RECIPE_DIR}/toolchain/cross-linux.cmake"
+    export TOOLCHAIN_FILE="cmake/cross-linux.cmake"
 fi
 
 ######
@@ -33,15 +24,8 @@ echo "PREFIX: "
 echo $PREFIX
 
 ######
-
-#echo "SDK dir:"
-#ls /Applications/Xcode_15.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/
-
 echo "Install conda-forge cxx compiler directly:"
 conda install --yes conda-forge::cxx-compiler
-
-#echo "g++ build.sh version:"
-#g++ --version
 
 cmake -S . \
   -B Release \
@@ -59,11 +43,11 @@ cmake --build Release --clean-first --target=install --verbose
 mkdir -p $PREFIX/etc/conda/activate.d
 mkdir -p $PREFIX/etc/conda/deactivate.d
 
-cp $RECIPE_DIR/activate.sh $PREFIX/etc/conda/activate.d/activate_${condaname}.sh
-cp $RECIPE_DIR/deactivate.sh $PREFIX/etc/conda/deactivate.d/deactivate_${condaname}.sh
+cp recipe/activate.sh $PREFIX/etc/conda/activate.d/activate_${condaname}.sh
+cp recipe/deactivate.sh $PREFIX/etc/conda/deactivate.d/deactivate_${condaname}.sh
 
-cp $RECIPE_DIR/activate.csh $PREFIX/etc/conda/activate.d/activate_${condaname}.csh
-cp $RECIPE_DIR/deactivate.csh $PREFIX/etc/conda/deactivate.d/deactivate_${condaname}.csh
+cp recipe/activate.csh $PREFIX/etc/conda/activate.d/activate_${condaname}.csh
+cp recipe/deactivate.csh $PREFIX/etc/conda/deactivate.d/deactivate_${condaname}.csh
 
 # Delete the cmake build directory
 rm -rf Release 
