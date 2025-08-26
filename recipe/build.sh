a5719d2a95bcd0223205527c460c7e5b706a10e1
jasercion <jasercion.gwu@gmail.com>
2025-08-26 14:36:48 -0400

    fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -36,7 +36,7 @@ else
         -B Release \
         -DCMAKE_C_COMPILER="$(which gcc)" \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_FILE} \
+        -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_FILE}" \
         -DCMAKE_PREFIX_PATH="${PREFIX}" \
         -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
         ${CMAKE_ARGS}
