a3bbf7c8d533079768117d97faabd76b17e260a9
jasercion <jasercion.gwu@gmail.com>
2025-09-29 13:27:18 -0400

    add forced -fPIC flag

--- recipe/build.sh
+++ recipe/build.sh
@@ -29,6 +29,7 @@ else
     echo "Configuring for Linux"
     cmake -S . \
         -B Release \
+        -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_CXX_FLAGS="-fPIC" \
         -DCMAKE_C_COMPILER="$(which gcc)" \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE \
